f787bad2b8045b0b77150cf59595113ebf1c4151
Evans Chibueze Okocha <x3mace007@gmail.com>
2024-12-22 17:21:30 +0100

    chore: add image crud

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,8 @@ export const SIGNUP_ROUTES = `${AUTH_ROUTES}/signup`;
 export const LOGIN_ROUTES = `${AUTH_ROUTES}/login`;
 export const GET_USER_INFO = `${AUTH_ROUTES}/user-info`;
 export const UPDATE_PROFILE_ROUTE = `${AUTH_ROUTES}/update-profile`;
+export const ADD_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/add-profile-image`;
+export const DELETE_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/delete-profile-image`;
 
 export const colors = [
   "bg-[#712c4a57] text-[#ff006e] border-[1px] border-[#ff006faa]",
