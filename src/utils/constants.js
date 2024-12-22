38b64dca98a29969668c8fd71ea9b41b3ab5f282
Evans Chibueze Okocha <x3mace007@gmail.com>
2024-12-22 09:53:37 +0100

    chore: add profile update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,7 @@ export const AUTH_ROUTES = "api/auth";
 export const SIGNUP_ROUTES = `${AUTH_ROUTES}/signup`;
 export const LOGIN_ROUTES = `${AUTH_ROUTES}/login`;
 export const GET_USER_INFO = `${AUTH_ROUTES}/user-info`;
+export const UPDATE_PROFILE_ROUTE = `${AUTH_ROUTES}/update-profile`;
 
 export const colors = [
   "bg-[#712c4a57] text-[#ff006e] border-[1px] border-[#ff006faa]",
